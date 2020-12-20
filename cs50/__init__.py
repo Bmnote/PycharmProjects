51a96b0e85cbb5ec46dabda648869191bac1c9d1
mac <mac@macdeMacBook-Pro.local>
2020-12-20 11:58:03 +0800

    update git.txt

---
+++ cs50/__init__.py
@@ -0,0 +1,13 @@
+'''
+0: HTML and CSS
+1: Git
+2: Python
+3: Django
+4: JavaScript
+5: Testing and CI(continuous integration 持续集成)/CD(continuous delivery 持续交付)
+6: Scalability and Security 扩展和安全性
+7:
+8:
+9:
+10:
+'''
